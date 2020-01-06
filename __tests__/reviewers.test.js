8e996227e50362df74f6068cc558d7d9b3f3aaf0
Travis Molter <travismolter@gmail.com>
2020-01-06 14:12:53 -0800

    Reviewers routes complete and tested

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Reviewer = require('../lib/Model/Reviewer');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -13,8 +14,41 @@ describe('app routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  let reviewer;
+  beforeEach(async() => {
+    reviewer = await  Reviewer.create({
+      name: 'treemo',
+      company: 'treemos reviews'
+    });
+  });
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('has a get all reviewers route', () => {
+    return request(app)
+      .get('/api/v1/reviewers')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: reviewer.id,
+          name: 'treemo',
+          company: 'treemos reviews',
+          __v: 0
+        }]);
+      });
+  });
+
+  it('has a get reviewer by id route', () => {
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewer.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: reviewer.id,
+          name: 'treemo',
+          company: 'treemos reviews',
+          __v: 0
+        });
+      });
   });
 });
