9b60136747bdd733fd9c52382f07f3e86767d7a3
Travis Molter <travismolter@gmail.com>
2020-01-06 13:22:10 -0800

    studio routes complete and tested

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Studio = require('../lib/Model/Studio');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -13,8 +14,53 @@ describe('app routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  let studio;
+  beforeEach(async() => {
+    studio = await  Studio.create({
+      name: 'Treemos Pictures',
+      address: {
+        city: 'Sherwood',
+        state: 'Oregon',
+        country: 'USA'
+      }
+    });
+  });
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('has a get all studios route', () => {
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: studio.id,
+          name: 'Treemos Pictures',
+          address: {
+            city: 'Sherwood',
+            state: 'Oregon',
+            country: 'USA'
+          },
+          __v: 0
+        }]);
+      });
+  });
+
+  it('has a get studio by id route', () => {
+    return request(app)
+      .get(`/api/v1/studios/${studio.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: studio.id,
+          name: 'Treemos Pictures',
+          address: {
+            city: 'Sherwood',
+            state: 'Oregon',
+            country: 'USA'
+          },
+          __v: 0
+        });
+      });
   });
 });
