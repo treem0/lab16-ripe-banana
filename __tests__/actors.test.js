5c4eb0d619869efdaf3ac2f955b5a5d5d5e9839f
Travis Molter <travismolter@gmail.com>
2020-01-06 13:35:47 -0800

    actor routes complete and tested

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Actor = require('../lib/Model/Actor');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -13,8 +14,44 @@ describe('app routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  let actor;
+  beforeEach(async() => {
+    actor = await  Actor.create({
+      name: 'Treemo Money',
+      dob: '1991-08-30',
+      pob: 'Pontiac, Michigan'
+    });
+  });
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('has a get all actors route', () => {
+    return request(app)
+      .get('/api/v1/actors')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: actor.id,
+          name: 'Treemo Money',
+          dob: '1991-08-30T00:00:00.000Z',
+          pob: 'Pontiac, Michigan',
+          __v: 0
+        }]);
+      });
+  });
+
+  it('has a get actor by id route', () => {
+    return request(app)
+      .get(`/api/v1/actors/${actor.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: actor.id,
+          name: 'Treemo Money',
+          dob: '1991-08-30T00:00:00.000Z',
+          pob: 'Pontiac, Michigan',
+          __v: 0
+        });
+      });
   });
 });
