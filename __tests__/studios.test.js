b22cce13f13673dc4149fb48080453e5d50bb687
Travis Molter <travismolter@gmail.com>
2020-01-06 17:10:12 -0800

    studio get by id route actually working this time

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Studio = require('../lib/Model/Studio');
+const Actor = require('../lib/Model/Actor');
+const Film = require('../lib/Model/Film');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -15,6 +17,8 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
   let studio;
+  let actor;
+  let films;
   beforeEach(async() => {
     studio = await  Studio.create({
       name: 'Treemos Pictures',
@@ -24,6 +28,20 @@ describe('app routes', () => {
         country: 'USA'
       }
     });
+    actor = await Actor.create({
+      name: 'treemo',
+      dob: '1991-08-30',
+      pob: 'Pontiac, Michigan'
+    });
+    films = await Film.create([{
+      title: 'Treemo Ninja',
+      studio: studio._id,
+      released: 2020,
+      cast: [{
+        role: 'best actor',
+        actor: actor._id
+      }]
+    }]);
   });
 
   afterAll(() => {
@@ -36,30 +54,25 @@ describe('app routes', () => {
       .then(res => {
         expect(res.body).toEqual([{
           _id: studio.id,
-          name: 'Treemos Pictures',
-          address: {
-            city: 'Sherwood',
-            state: 'Oregon',
-            country: 'USA'
-          },
-          __v: 0
+          name: 'Treemos Pictures'
         }]);
       });
   });
 
   it('has a get studio by id route', () => {
     return request(app)
-      .get(`/api/v1/studios/${studio.id}`)
+      .get(`/api/v1/studios/${studio._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          _id: studio.id,
+          _id: studio._id.toString(),
+          __v: 0,
           name: 'Treemos Pictures',
           address: {
             city: 'Sherwood',
             state: 'Oregon',
             country: 'USA'
           },
-          __v: 0
+          films: films.map(film => ({ _id: expect.any(String), title: film.title, studio: studio.id.toString() }))
         });
       });
   });
