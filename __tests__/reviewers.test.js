497529e883207717912763dab7c538a2408e6ca8
Travis Molter <travismolter@gmail.com>
2020-01-07 16:18:52 -0800

    reviewers get by id route is complete and tested

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -5,6 +5,10 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Reviewer = require('../lib/Model/Reviewer');
+const Review = require('../lib/Model/Review');
+const Actor = require('../lib/Model/Actor');
+const Studio = require('../lib/Model/Studio');
+const Film = require('../lib/Model/Film');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,12 +18,47 @@ describe('app routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  let studio;
+  let actor;
+  let films;
+  let reviews;
   let reviewer;
   beforeEach(async() => {
-    reviewer = await  Reviewer.create({
-      name: 'treemo',
-      company: 'treemos reviews'
+    studio = await  Studio.create({
+      name: 'Treemos Pictures',
+      address: {
+        city: 'Sherwood',
+        state: 'Oregon',
+        country: 'USA'
+      }
     });
+    actor = await Actor.create({
+      name: 'treemo',
+      dob: '1991-08-30',
+      pob: 'Pontiac, Michigan'
+    });
+    films = await Film.create([{
+      title: 'Treemo Ninja',
+      studio: studio._id,
+      released: 2020,
+      cast: [{
+        role: 'best actor',
+        actor: actor._id
+      }]
+    }]);
+    reviewer = await Reviewer.create({
+      name: 'tmo',
+      company: 'treesus reviews'
+    });
+    reviews = await Review.create([{
+      rating: 5,
+      reviewer: reviewer._id,
+      review: 'this movie sucked',
+      film: {
+        _id: films[0]._id,
+        title: films[0].title
+      }
+    }]);
   });
 
   afterAll(() => {
@@ -32,8 +71,8 @@ describe('app routes', () => {
       .then(res => {
         expect(res.body).toEqual([{
           _id: reviewer.id,
-          name: 'treemo',
-          company: 'treemos reviews',
+          name: 'tmo',
+          company: 'treesus reviews',
           __v: 0
         }]);
       });
@@ -45,9 +84,17 @@ describe('app routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: reviewer.id,
-          name: 'treemo',
-          company: 'treemos reviews',
-          __v: 0
+          name: 'tmo',
+          company: 'treesus reviews',
+          reviews: [{
+            _id: reviews[0]._id.toString(),
+            rating: 5,
+            review: 'this movie sucked',
+            film: {
+              _id: films[0].id.toString(),
+              title: films[0].title
+            }
+          }]
         });
       });
   });
