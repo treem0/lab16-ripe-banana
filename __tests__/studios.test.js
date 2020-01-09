92416701ef688e614c7e96dba35ea055176b821d
Travis Molter <travismolter@gmail.com>
2020-01-08 17:45:10 -0800

    almost done with studio routes with helper

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -1,78 +1,28 @@
-require('dotenv').config();
+const { getStudio, getStudios } = require('../lib/helpers/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-const Studio = require('../lib/Model/Studio');
-const Actor = require('../lib/Model/Actor');
-const Film = require('../lib/Model/Film');
+
 
 describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-  let studio;
-  let actor;
-  let films;
-  beforeEach(async() => {
-    studio = await  Studio.create({
-      name: 'Treemos Pictures',
-      address: {
-        city: 'Sherwood',
-        state: 'Oregon',
-        country: 'USA'
-      }
-    });
-    actor = await Actor.create({
-      name: 'treemo',
-      dob: '1991-08-30',
-      pob: 'Pontiac, Michigan'
-    });
-    films = await Film.create([{
-      title: 'Treemo Ninja',
-      studio: studio._id,
-      released: 2020,
-      cast: [{
-        role: 'best actor',
-        actor: actor._id
-      }]
-    }]);
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
-  it('has a get all studios route', () => {
+  it('has a get all studios route', async() => {
+    const studios = await getStudios();
     return request(app)
       .get('/api/v1/studios')
       .then(res => {
-        expect(res.body).toEqual([{
-          _id: studio.id,
-          name: 'Treemos Pictures'
-        }]);
+        expect(res.body).toEqual(studios.map(studio => ({
+          _id: studio._id, 
+          name: studio.name 
+        })));
       });
   });
 
-  it('has a get studio by id route', () => {
+  it('has a get studio by id route', async() => {
+    const studio = await getStudio();
     return request(app)
       .get(`/api/v1/studios/${studio._id}`)
       .then(res => {
-        expect(res.body).toEqual({
-          _id: studio._id.toString(),
-          name: 'Treemos Pictures',
-          address: {
-            city: 'Sherwood',
-            state: 'Oregon',
-            country: 'USA'
-          },
-          films: films.map(film => ({ _id: expect.any(String), title: film.title }))
-        });
+        expect(res.body).toEqual({ studio });
       });
   });
 });
